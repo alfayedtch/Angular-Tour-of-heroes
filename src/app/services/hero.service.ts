d141ce26e8dbf3425af9e83666d48a787634323d
Al-Fayed Tchagnao <atchagna@pc-gi-593.utbm.local>
2021-03-11 11:15:51 +0100

    Show message | ajouter message

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from '../Models/hero';
 import { HEROES } from '../mock-heroes';
 import { Observable,of } from 'rxjs';
+import { MessageService } from './message.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService:MessageService) { }
 
 
   getHeroes():Observable<Hero[]>{
     const lesheroes = of(HEROES);
+    this.messageService.add('Heroservice:fetched heroes');
     return lesheroes;
   }
 }
