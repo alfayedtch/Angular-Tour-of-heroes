ab948b2f9f48ca57114ce0322b068aebf2397a0b
Al-Fayed Tchagnao <atchagna@pc-gi-593.utbm.local>
2021-03-11 11:26:07 +0100

    Ajout de message aditionnels au service de hero | add aditionnal message to hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Models/hero';
 import { HeroService} from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +13,7 @@ export class HeroesComponent implements OnInit {
 
   heroes:Hero[] = [];
 
-  constructor(private heroService:HeroService) { }
+  constructor(private heroService:HeroService,private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -20,7 +21,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero:Hero){
     this.selectedhero = hero;
+    this.messageService.add(`HeroesComponent: Id du héro Selectionné =${hero.id}`);
   }
+
   getHeroes():void{
     this.heroService.getHeroes().subscribe(heroes => this.heroes=heroes);
   }
